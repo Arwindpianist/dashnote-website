e88e980fd1996a706fc6602be645c0057551adc4
arwindpianist <arwindpianist@gmail.com>
2023-11-11 23:42:02 +0800

    vue-config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/dashnote-website/" : "/",
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dashnote-website/'
+    : '/'
+}
